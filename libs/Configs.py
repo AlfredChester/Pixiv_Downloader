05f445b0a22d8b39e3587a7870e95a8120e7ffb5
FredB-mine <fredbao1126@gmail.com>
2022-08-21 14:23:46 +0800

    Downloader configs

---
+++ libs/Configs.py
@@ -0,0 +1,12 @@
+class Configs:
+    __name__ = "Configs"
+    class logConfigs:
+        needConsoleOutPut   = True
+        needFileOutPut      = True
+        loglevel            = "DEBUG"
+        fileName            = "downloadProcess.log"
+        fileEncoding        = "utf-8"
+
+    class timeConfigs:
+        timeFormat          = "%Y-%m-%d [%H:%M:%S] "
+        timeZone            = "Asia/Shanghai"
