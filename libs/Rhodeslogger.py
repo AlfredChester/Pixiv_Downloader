688680caf5f0b7a0c1068c79a983f273b581e37b
FredB-mine <fredbao1126@gmail.com>
2022-08-21 14:24:04 +0800

    Rhodes log framework

---
+++ libs/Rhodeslogger.py
@@ -0,0 +1,50 @@
+import time
+from loguru import logger
+from typing import Any
+from libs.Configs import Configs
+
+def getNowTime() -> str:
+    return time.strftime(
+        Configs.timeConfigs.timeFormat, time.localtime()
+    )
+
+def writeLog(data: Any, logType:str = "INFO") -> None:
+    logObject = open(
+        Configs.logConfigs.fileName, "a" ,
+        encoding = Configs.logConfigs.fileEncoding
+    )
+    funcMap = {
+        "INFO":     logger.info,
+        "DEBUG":    logger.debug,
+        "ERROR":    logger.error,
+        "CRITICAL": logger.critical,
+        "WARNING":  logger.warning,
+        "SUCCESS":  logger.success,
+        "TRACE":    logger.trace
+    }
+    toUseFunc  = funcMap[logType]
+    if type(data) == list: 
+        for item in data:
+            item = item.replace("\n", "")
+            if (Configs.logConfigs.needFileOutPut):
+                logObject.write(
+                    getNowTime() + str(item) + '\n'
+                )
+            if (Configs.logConfigs.needConsoleOutPut):
+                toUseFunc(str(item))
+        logObject.close()
+        return
+
+    if type(data) == str:
+        data = data.replace("\n", "")
+        if (Configs.logConfigs.needFileOutPut):
+            logObject.write(getNowTime() + data + '\n')
+        if (Configs.logConfigs.needConsoleOutPut):
+            toUseFunc(data)
+        logObject.close()
+        return
+
+    else:
+        logObject.close()
+        writeLog("Error: logType is not supported", "ERROR")
+        raise TypeError("Data type not supported")
